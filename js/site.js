a92f2314cb00b0076d4543d836d849c64fcd0668
duncanbain <pidginperfect@Pidgins-MacBook-Pro.local>
2014-07-09 15:45:23 +0100

    Initial changes
    Change of placeholder text and map origin

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 var findme_map = L.map('findme-map')
-    .setView([37.7, -97.3], 3),
-    osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    .setView([55.8638, -4.2493], 11),
+    osmUrl = 'https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png',
     osmAttrib = 'Map data © OpenStreetMap contributors',
     osm = L.tileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib}).addTo(findme_map),
     category_data = [];
